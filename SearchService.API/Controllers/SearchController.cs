aa01cec0c5b2f4a5250238c9955646b8ce75da1d
jisaacdavid <julio.david@cit.hn>
2024-05-17 14:52:38 -0600

    section 2 done. section 3 in progress. added mongo , seeding and search functionality

---
+++ SearchService.API/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.API.Models;
+
+namespace SearchService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+        {
+            var query = DB.Find<Item>();
+
+            query.Sort(x => x.Ascending(a => a.Make));
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                query.Match(Search.Full, searchTerm).SortByTextScore();
+            }
+
+            var result = await query.ExecuteAsync();
+
+            return result;
+        }
+    }
+}
