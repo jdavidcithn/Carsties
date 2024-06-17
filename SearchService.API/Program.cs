337bb95cab561629f9193bc7eaf3d72873638f01
jisaacdavid <julio.david@cit.hn>
2024-06-17 15:33:06 -0600

    end of section 3.

--- SearchService.API/Program.cs
+++ SearchService.API/Program.cs
@@ -1,13 +1,17 @@
+using System.Net;
 using MongoDB.Driver;
-using MongoDB.Entities;
+using MongoDB.Entities;using Polly;
+using Polly.Extensions.Http;
 using SearchService.API.Data;
 using SearchService.API.Models;
+using SearchService.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 
 var app = builder.Build();
@@ -16,13 +20,22 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() => HttpPolicyExtensions
+                                                        .HandleTransientHttpError()
+                                                        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+                                                        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
